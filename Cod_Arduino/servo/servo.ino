290db9b3c3d42058319b307db15ce4a000e777ec
sandraGonzalezP <32445461+sandraGonzalezP@users.noreply.github.com>
2017-12-11 14:47:22 -0600

    Add files via upload

---
+++ Cod_Arduino/servo/servo.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+ 
+Servo myservo;  // crea el objeto servo
+ 
+int vel = 0;    // velocidad del servo
+ 
+void setup() {
+   myservo.attach(9);  // vincula el servo al pin digital 9
+}
+ 
+void loop() {
+  
+   //servo 100% CW (equivalente a angulo 180º)
+   vel = 180;
+   myservo.write(vel);              
+   delay(1500); 
+ 
+   //servo 100% CCW (equivalente a angulo 0º)
+   vel = 0;
+   myservo.write(vel);              
+   delay(1500); 
+}
